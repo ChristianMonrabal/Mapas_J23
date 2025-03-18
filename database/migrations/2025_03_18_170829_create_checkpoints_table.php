5db9c90dfbc2ff3bf44b3992986fdcb068f7e1dd
Marcolo24 <100005655.joan23@fje.edu>
2025-03-18 18:18:45 +0100

    migrations

---
+++ database/migrations/2025_03_18_170829_create_checkpoints_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('checkpoints', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('gymkhana_id');
+            $table->foreign('gymkhana_id')->references('id')->on('gymkhanas');
+            $table->unsignedBigInteger('place_id');
+            $table->foreign('place_id')->references('id')->on('places');
+            $table->text('pista');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('checkpoints');
+    }
+};
---
+++ database/migrations/2025_03_18_171149_create_gymkhana_progress_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gymkhana_progress', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('checkpoint_id');
+            $table->foreign('checkpoint_id')->references('id')->on('checkpoints');
+            $table->boolean('completed')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gymkhana_progress');
+    }
+};
