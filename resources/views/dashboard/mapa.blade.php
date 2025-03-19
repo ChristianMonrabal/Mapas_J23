bb2b7222b2e60a9e6a871a9beddae1e22b768a93
Marcolo24 <100005655.joan23@fje.edu>
2025-03-19 19:27:52 +0100

    estilos mapa

--- resources/views/dashboard/mapa.blade.php
+++ resources/views/dashboard/mapa.blade.php
@@ -3,6 +3,23 @@
 @section('title', 'Mapa')
 
 @section('content')
+
+<div class="container">
+
+</div>
+
+<div class="search-bar">
+    <button id="sidebar-btn" class="sidebar-btn">
+        <i class="fas fa-bars"></i>
+    </button>    
+    <input type="text" id="searchInput" placeholder="Busca un sitio">
+</div>
+
+<div class="sidebar" id="sidebar">
+<button class="btn btn-primary">Hola</button>
+    <!-- Aquí puedes añadir el contenido del sidebar -->
+</div>
+
 <div id="map"></div>
 
 @endsection
--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -14,6 +14,14 @@
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
     crossorigin=""></script>
+    <!-- CSS de Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <!-- JavaScript de Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 
     <!-- Estilos personalizados -->
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
