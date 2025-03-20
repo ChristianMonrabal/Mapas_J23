8e588fbf4e3bd7ac3aacead5a091b914d1cef32a
pol-mrk <polmarc.monro@gmail.com>
2025-03-20 01:08:07 +0100

    seeders_tags_places

--- database/seeders/PlaceSeeder.php
+++ database/seeders/PlaceSeeder.php
@@ -16,12 +16,132 @@ class PlaceSeeder extends Seeder
     public function run(): void
     {
         $now = Carbon::now();
-
+        
         Place::create([
-            'name' => 'Bar Alonso',
-            'description' => 'bar de tapas y bocatas',
+            'name' => "Alonso's Cafe",
+            'address' => "Av. Mare de Déu de Bellvitge, 86, 08907 L'Hospitalet de Llobregat, Barcelona",
+            'latitude' => 41.3492394,
+            'longitude' => 2.1049299,
             'created_at' => $now,
             'updated_at' => $now,
         ]);
+        
+        Place::create([
+            'name' => 'Ermita Sta Mª de Bellvitge',
+            'address' => "Carrer de l'Ermita de Bellvitge, 6, 08907 L'Hospitalet de Llobregat, Barcelona",
+            'latitude' => 41.3485316,
+            'longitude' => 2.1083162,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+        
+        Place::create([
+            'name' => 'Parque de Bellvitge',
+            'address' => "Carrer de l'Ermita de Bellvitge, 38, 08907 L'Hospitalet de Llobregat, Barcelona",
+            'latitude' => 41.348399,
+            'longitude' => 2.1085102,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+        
+        Place::create([
+            'name' => 'Centre Cultural Bellvitge-Gornal',
+            'address' => "Plaça de la Cultura, 1, 08907 L'Hospitalet de Llobregat, Barcelona",
+            'latitude' => 41.3512486,
+            'longitude' => 2.109487,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+        
+        Place::create([
+            'name' => 'Pista de futsal',
+            'address' => "Av. d'Amèrica, 117B, 08907 L'Hospitalet de Llobregat, Barcelona",
+            'latitude' => 41.3523883,
+            'longitude' => 2.1125246,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+        
+        Place::create([
+            'name' => 'Pirulo Bellvitge',
+            'address' => "Av. d'Amèrica, 117B, 08907 L'Hospitalet de Llobregat, Barcelona",
+            'latitude' => 41.3527157,
+            'longitude' => 2.1117523,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+        
+        Place::create([
+            'name' => 'Petanca de Bellvitge',
+            'address' => "08907 L'Hospitalet de Llobregat, Barcelona",
+            'latitude' => 41.3534352,
+            'longitude' => 2.112784,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        Place::create([
+            'name' => 'Xurreria La Confiança',
+            'address' => "Av. d'Europa, 29, 08907 L'Hospitalet de Llobregat, Barcelona",
+            'latitude' => 41.3527157,
+            'longitude' => 2.1117523,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        Place::create([
+            'name' => 'Estación de tren Bellvitge',
+            'address' => "Av. d'Amèrica, 08907 L'Hospitalet de Llobregat, Barcelona",
+            'latitude' => 41.3549096,
+            'longitude' => 2.1127199,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        Place::create([
+            'name' => 'Polideportivo Municipal Bellvitge Sergio Manzano',
+            'address' => "Av. Mare de Déu de Bellvitge, 7, 08907 L'Hospitalet de Llobregat, Barcelona",
+            'latitude' => 41.3499005,
+            'longitude' => 2.1047223,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        Place::create([
+            'name' => 'Caprabo',
+            'address' => "Av. Mare de Déu de Bellvitge, 40, 08907 L'Hospitalet de Llobregat, Barcelona",
+            'latitude' => 41.3483958,
+            'longitude' => 2.1077026,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        Place::create([
+            'name' => 'Tabacs Exp. nº 21',
+            'address' => "Carrer de l'Ermita de Bellvitge, 47, Local A, 08907 L'Hospitalet de Llobregat, Barcelona",
+            'latitude' => 41.3482729,
+            'longitude' => 2.1078789,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        Place::create([
+            'name' => 'Consultas Externas Hospital de Bellvitge',
+            'address' => "Carrer de la Feixa Llarga, s/n, 08907 L'Hospitalet de Llobregat, Barcelona",
+            'latitude' => 41.347926,
+            'longitude' => 2.1081551,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        Place::create([
+            'name' => 'BELLVITGE PITA KEBAB',
+            'address' => "Passeig de Bellvitge, 08907 L'Hospitalet de Llobregat, Barcelona",
+            'latitude' => 41.3478866,
+            'longitude' => 2.1057738,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+        
     }
 }
--- database/seeders/PlaceTagSeeder.php
+++ database/seeders/PlaceTagSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\PlaceTag;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,104 @@ class PlaceTagSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $now = Carbon::now();
+
+        PlaceTag::create([
+            'place_id' => 1,
+            'tag_id' => 2,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        PlaceTag::create([
+            'place_id' => 2,
+            'tag_id' => 13,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        PlaceTag::create([
+            'place_id' => 3,
+            'tag_id' => 4,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        PlaceTag::create([
+            'place_id' => 4,
+            'tag_id' => 6,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        PlaceTag::create([
+            'place_id' => 5,
+            'tag_id' => 12,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        PlaceTag::create([
+            'place_id' => 6,
+            'tag_id' => 5,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        PlaceTag::create([
+            'place_id' => 7,
+            'tag_id' => 4,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        PlaceTag::create([
+            'place_id' => 8,
+            'tag_id' => 11,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        PlaceTag::create([
+            'place_id' => 9,
+            'tag_id' => 10,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        PlaceTag::create([
+            'place_id' => 10,
+            'tag_id' => 9,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        PlaceTag::create([
+            'place_id' => 11,
+            'tag_id' => 8,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        PlaceTag::create([
+            'place_id' => 12,
+            'tag_id' => 7,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        PlaceTag::create([
+            'place_id' => 13,
+            'tag_id' => 3,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        PlaceTag::create([
+            'place_id' => 14,
+            'tag_id' => 14,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
     }
 }
