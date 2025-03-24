83d04dbfc7b64ffef735ce8335a6d8581254b8bf
pol-mrk <polmarc.monro@gmail.com>
2025-03-24 16:20:34 +0100

    mapas16

---
+++ resources/views/dashboard/gimcana.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.layout')
+
+@section('title', 'Mapa')
+
+@section('content')
+
+
+
+@endsection
+
+@section('scripts')
+<script src="{{ asset('js/mapa.js') }}"></script>
+@endsection
